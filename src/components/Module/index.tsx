5de299f2c8b03b72cd3b0ae7d20b034369a930d0
GuiLeoni14 <guileone167@gmail.com>
2023-06-30 13:39:57 -0300

    chore

--- src/components/Module/index.tsx
+++ src/components/Module/index.tsx
@@ -1,8 +1,7 @@
 import * as Collapsible from '@radix-ui/react-collapsible'
-import { useDispatch } from 'react-redux'
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from '../Lesson'
-import { useAppSelector } from '../../store'
+import { useAppDispatch, useAppSelector } from '../../store'
 import { play } from '../../store/slices/player'
 
 interface ModuleProps {
@@ -12,13 +11,13 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
   const { currentLessonIndex, currentModuleIndex } = useAppSelector((state) => {
     const { currentLessonIndex, currentModuleIndex } = state.player
     return { currentLessonIndex, currentModuleIndex }
   })
   const lessons = useAppSelector((state) => {
-    return state.player.course.modules[moduleIndex].lessons
+    return state.player.course?.modules[moduleIndex].lessons
   })
 
   return (
@@ -38,7 +37,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
+          {lessons?.map((lesson, lessonIndex) => {
             const isCurrent =
               currentModuleIndex === moduleIndex &&
               currentLessonIndex === lessonIndex
