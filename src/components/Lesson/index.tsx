5b11473663127f8fbaa106e57b06f3123b7b5e19
GuiLeoni14 <guileone167@gmail.com>
2023-06-30 22:03:36 -0300

    feat: test

--- src/components/Lesson/index.tsx
+++ src/components/Lesson/index.tsx
@@ -1,4 +1,5 @@
 import { PlayCircle, Video } from 'lucide-react'
+
 interface LessonProps {
   title: string
   duration: string
@@ -20,11 +21,10 @@ export function Lesson({
       className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
     >
       {isCurrent ? (
-        <PlayCircle className="w-4 h-4" />
+        <PlayCircle className="w-4 h-4 text-emerald-400" />
       ) : (
         <Video className="w-4 h-4 text-zinc-500" />
       )}
-
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">
         {duration}
--- src/components/Module/index.tsx
+++ src/components/Module/index.tsx
@@ -1,8 +1,8 @@
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from 'lucide-react'
+
 import { Lesson } from '../Lesson'
-import { useAppDispatch, useAppSelector } from '../../store'
-import { play } from '../../store/slices/player'
+import { useStore } from '../../zustand-store'
 
 interface ModuleProps {
   moduleIndex: number
@@ -11,14 +11,16 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useAppDispatch()
-  const { currentLessonIndex, currentModuleIndex } = useAppSelector((state) => {
-    const { currentLessonIndex, currentModuleIndex } = state.player
-    return { currentLessonIndex, currentModuleIndex }
-  })
-  const lessons = useAppSelector((state) => {
-    return state.player.course?.modules[moduleIndex].lessons
-  })
+  const { currentLessonIndex, currentModuleIndex, play, lessons } = useStore(
+    (store) => {
+      return {
+        lessons: store.course?.modules[moduleIndex].lessons,
+        currentLessonIndex: store.currentLessonIndex,
+        currentModuleIndex: store.currentModuleIndex,
+        play: store.play,
+      }
+    },
+  )
 
   return (
     <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
@@ -37,21 +39,22 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons?.map((lesson, lessonIndex) => {
-            const isCurrent =
-              currentModuleIndex === moduleIndex &&
-              currentLessonIndex === lessonIndex
+          {lessons &&
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent =
+                currentModuleIndex === moduleIndex &&
+                currentLessonIndex === lessonIndex
 
-            return (
-              <Lesson
-                key={lesson.id}
-                title={lesson.title}
-                duration={lesson.duration}
-                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-                isCurrent={isCurrent}
-              />
-            )
-          })}
+              return (
+                <Lesson
+                  key={lesson.id}
+                  title={lesson.title}
+                  duration={lesson.duration}
+                  isCurrent={isCurrent}
+                  onPlay={() => play([moduleIndex, lessonIndex])}
+                />
+              )
+            })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
